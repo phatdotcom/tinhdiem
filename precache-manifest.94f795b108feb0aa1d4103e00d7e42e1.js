8f1291cfe99074a2136675f74dc0589b5becb8d9
PCC <mayvagioios@gmail.com>
2020-08-23 19:25:52 +0700

    Updates

---
+++ precache-manifest.94f795b108feb0aa1d4103e00d7e42e1.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc6a92e58ded01194e3c3c2f1dd27b84",
+    "url": "/tinhdiem/index.html"
+  },
+  {
+    "revision": "1c2c6172ab927c3f2ca2",
+    "url": "/tinhdiem/static/css/2.97ca6098.chunk.css"
+  },
+  {
+    "revision": "294ec097bb10a038c418",
+    "url": "/tinhdiem/static/css/main.a9464ea3.chunk.css"
+  },
+  {
+    "revision": "1c2c6172ab927c3f2ca2",
+    "url": "/tinhdiem/static/js/2.be9bd232.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/tinhdiem/static/js/2.be9bd232.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "294ec097bb10a038c418",
+    "url": "/tinhdiem/static/js/main.3e71804b.chunk.js"
+  },
+  {
+    "revision": "a7377c6779e395825bfb",
+    "url": "/tinhdiem/static/js/runtime-main.2abe6e17.js"
+  },
+  {
+    "revision": "cc4da3a4d00f61df4b6b780bacfb7cec",
+    "url": "/tinhdiem/static/media/new.cc4da3a4.svg"
+  },
+  {
+    "revision": "d8bcd7363cc54b197c9861d25de8047c",
+    "url": "/tinhdiem/static/media/pics2.d8bcd736.jpg"
+  },
+  {
+    "revision": "f8126a1669e6c1556ef23f0b08e23532",
+    "url": "/tinhdiem/static/media/pics3.f8126a16.png"
+  },
+  {
+    "revision": "1551571955d541ce83cae8e772382ba1",
+    "url": "/tinhdiem/static/media/star.15515719.svg"
+  }
+]);
